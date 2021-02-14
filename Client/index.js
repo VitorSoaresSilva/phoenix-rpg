64332d0ff593ee4cd63569dcc5a0261f0b2e6aef
vitorsoaressilva <vitorcabeca2005@hotmail.com>
2021-02-14 10:40:30 -0300

    Join room/Utils Func/Disconnect/Random position

--- Client/index.js
+++ Client/index.js
@@ -4,31 +4,51 @@ const btnNewGame = document.getElementById('btnNewGame');
 
 const loginOuterContainer = document.getElementById('loginOuterContainer');
 const homeOuterContainer = document.getElementById('homeOuterContainer');
+const tableRoomNames = document.getElementById('tableRoomNames');
+const btnGetData = document.getElementById('btnGetData');
 
 btnNewGame.addEventListener('click',()=>socket.emit('newRoom'))
+btnGetData.addEventListener('click',()=>socket.emit('getData'))
 
 let canvas,ctx;
-
+socket.emit('getRoomNames')
 
 socket.on('roomData',receiveRoomData);
-// socket.on('init',initGame)
+socket.on('roomNames',receiveRoomNames);
+socket.on('unknownGame',unknownGame);
+socket.on('newPlayer',getPlayer);
+socket.on('data',(data)=>{console.log(data)});
 
-
-
-
+function getPlayer(playerData){
+    console.log(playerData);
+}
 
 function receiveRoomData(roomData){
     homeOuterContainer.style.display = 'flex';
     loginOuterContainer.style.display = 'none';
-
-
+    console.log(roomData)
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     canvas.width = canvas.height = roomData.canvasSize;
     ctx.fillStyle = roomData.canvasColor;
     ctx.fillRect(0,0,canvas.width,canvas.height)
+}
 
-}   
-// function initGame(){
-
-// }
+function receiveRoomNames(roomNames){
+    tableRoomNames.innerHTML = '';
+    roomNames.map( name => {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('class','btn btn-info');
+        btn.innerHTML = 'Entrar'
+        btn.addEventListener('click',()=>socket.emit('joinRoom',name))
+        var row = tableRoomNames.insertRow(0)
+        var cell1 = row.insertCell(0)
+        var cell2 = row.insertCell(1)
+        cell1.innerHTML = name;
+        cell2.appendChild(btn);
+    })
+}
+function unknownGame(){
+    alert("Esta sala nao existe mais")
+}
--- Server/server.js
+++ Server/server.js
@@ -5,44 +5,137 @@ const io = require('socket.io')(http, {
         methods: ['GET', 'POST']
     }
 })
-const { makeId } = require('./utils')
-const { GRID_SIZE,CANVAS_SIZE, FRAME_RATE } = require('./constants')
+const {makeId} = require('./utils')
+const {GRID_SIZE, CANVAS_SIZE, FRAME_RATE} = require('./constants')
 
-io.listen(PORT,()=>{
+io.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
 
-const rooms = [];
+let rooms = [];
+let players = [];
 
-io.on('connection',client => {
+io.on('connection', client => {
+    let myRoom = '';
     console.log('New connection')
-    client.emit('init','Hello world from Server')
-    client.on('newRoom', handleNewRoom)
+    client.emit('init', 'Hello world from Server')
+    client.on('newRoom', handleNewRoom);
+    client.on('getRoomNames', handleGetRooms);
+    client.on('joinRoom', handleJoinRoom);
 
+    function handleConnectToRoom(roomName) {
+        client.join(roomName)
+        myRoom = roomName;
+        let room = getRoomByName(myRoom)
+        client.emit('roomData', room)
+        // console.log('myrom',myRoom)
+        client.emit('newPlayer', createNewPlayer(myRoom));
+    }
 
-
-
-    function handleNewRoom(){
+    function handleNewRoom() {
         // cria um nome unico
         const name = makeId(5);
-        rooms.map(room=>{
-            if(room.name === name){
+        rooms.map(room => {
+            if (room.name === name) {
                 handleNewRoom();
             }
         })
-
         //cria o estado inicial da room
         const newRoom = {
             name: name,
             gridSize: GRID_SIZE,
             canvasSize: CANVAS_SIZE,
+            size: CANVAS_SIZE/GRID_SIZE,
             canvasColor: '#004545',
+            curLevel: 1
         }
-
         //adiciona a room à lista
         rooms.push(newRoom);
-
         //envia para o cliente os dados
-        client.emit('roomData',newRoom)
+        updateRooms();
+        handleConnectToRoom(newRoom.name)
     }
+    async function handleJoinRoom(roomName) {
+        let ids = await io.in(roomName).allSockets()
+        if (ids.size === 0) {
+            client.emit('unknownGame')
+            updateRooms();
+            return;
+        }
+        handleConnectToRoom(roomName);
+    }
+
+    function handleGetRooms() {
+        const allRoomNames = rooms.map(room => room.name)
+        client.emit('roomNames', allRoomNames);
+    }
+
+    function updateRooms() {
+        const allRoomNames = rooms.map(room => room.name)
+        client.broadcast.emit('roomNames', allRoomNames);
+    }
+
+    function createNewPlayer(roomName) {
+        const newPlayer = {
+            id: client.id,
+            pos: getRandomPosition(),
+            vel: {x: 0,y: 0},
+            rot: {x: 0,y: 0},
+            room: roomName
+        }
+        players.push(newPlayer);
+        return newPlayer;
+    }
+
+    function getPlayer(clientId = client.id) {
+        return players.filter(player => player.id === clientId)[0];
+    }
+
+    function getPlayersInRoom(roomName) {
+        return players.filter(player => player.room === roomName)
+    }
+
+    function getRoomByName(roomName) {
+        return rooms.filter(room => room.name === roomName)[0];
+    }
+    function getRandomPosition() {
+        let room = getRoomByName(myRoom);
+        // ver se nao tem nenhum player
+        // ver se nao tem obstaculo
+        const newPos = {
+            x: Math.round(Math.random() * room.gridSize),
+            y: Math.round(Math.random() * room.gridSize)
+        }
+        if(!isValidPosition(newPos)){
+            getRandomPosition()
+        }
+        return newPos;
+    }
+    function isValidPosition(pos){
+        let arrPlayers = getPlayersInRoom(myRoom);
+        arrPlayers.map(player=>{
+            if(pos.x === player.pos.x && pos.y === player.pos.y){
+                return false
+                // return {obstaculo: player}
+            }
+        })
+        return true;
+    }
+
+
+    client.on('disconnect', () => {
+        let player = getPlayer()
+        //socket tinha um player?
+        if (player) {
+            //salvo a sala que ele tava
+            let roomName = player.room;
+            //removo o player dos players
+            players = players.filter(x => x.id != player.id);
+            //verifico se a sala ficou vazia
+            if (getPlayersInRoom(roomName).length === 0) {
+                rooms = rooms.filter(room => room.name !== roomName);
+                updateRooms();
+            }
+        }
+    })
 })
